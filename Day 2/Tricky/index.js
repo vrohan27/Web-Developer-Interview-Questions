cb75968f11c5c5eaacdf958ebd812c2a932166db
vrohan27 <vrohan010@gmail.com>
2024-04-06 18:27:31 +0530

    Practice of Day 2

---
+++ Day 2/Tricky/index.js
@@ -0,0 +1,65 @@
+// quz 1
+// Guess the answer
+
+// function newFuntion(){
+//     console.log('Hello world');
+// }
+
+// console.log(newFuntion.name);
+
+
+//----------------------------------
+
+// quz 2
+// guess the answer
+// const obj = Object.create(null);
+// const keyToCheck = "randomKey";
+
+// if(obj.hasOwnProperty(keyToCheck)){
+//     console.log('key found');
+// }else{
+//     console.log('key not found');
+// }
+
+
+//---------------------------------
+
+// quz 3
+
+// const obj = {};
+// const key = 'constructor';
+
+// if(key in obj){
+//     console.log('key found');
+// }else{
+//     console.log('key not found.');
+// }
+
+//solution
+// const obj = {};
+// const key = 'constructor';
+
+// if(Object.hasOwn(obj,key)){
+//     console.log('key found');
+// }else{
+//     console.log('key not found.');
+// }
+
+
+//----------------------------------
+
+// quz 4
+
+// const number = 21;
+
+// console.log(number.toString());
+// console.log(number.toString(2));
+
+//-----------------------------------
+
+// quz 5
+
+// const promise = new Promise((_, reject)=>{reject()});
+
+// const res = promise.then(()=>{console.log('then')}).catch(()=>{console.log('catch')}).then((data)=>{console.log('then after catch')});
+
